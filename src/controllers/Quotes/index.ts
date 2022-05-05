eaa5799e14c91a0e7cd0b8a55962581b03d8b2dc
Estanip <estani.pettigrew@gmail.com>
2022-05-04 23:20:56 -0300

    improve getaverage

--- src/controllers/Quotes/index.ts
+++ src/controllers/Quotes/index.ts
@@ -3,6 +3,81 @@ const puppeteer = require('puppeteer');
 
 require('dotenv').config();
 
+
+/* export const getQuote = async (req: { query: { source: any; }; }, res: any) => {
+
+    const { source } = req.query;
+
+    try {
+
+        const browser = await puppeteer.launch({
+            args: ['--no-sandbox']
+        });
+
+        const page = await browser.newPage();
+        await page.setDefaultNavigationTimeout(0);
+
+        let buyPrice;
+        let sellPrice;
+        let url;
+
+        let buy_price;
+        let sell_price;
+
+        if (source === 'ambito') {
+
+            await page.goto('https://www.ambito.com/contenidos/dolar.html');
+            url = await page.url();
+
+            buyPrice = await page.$$eval('[data-indice*="/dolar/informal"] span.value.data-compra', (elements: any[]) => elements.map((el: { innerText: any; }) => el.innerText))
+            sellPrice = await page.$$eval('[data-indice*="/dolar/informal"] span.value.data-venta', (elements: any[]) => elements.map((el: { innerText: any; }) => el.innerText))
+
+            buy_price = parseFloat(buyPrice[0].replace(',', '.'))
+            sell_price = parseFloat(sellPrice[0].replace(',', '.'))
+
+        } else if (source === 'dolarhoy') {
+
+            await page.goto('https://www.dolarhoy.com');
+            url = await page.url();
+
+            buyPrice = await page.$$eval('.tile.is-parent.is-5 div.values div.compra div.val', (elements: any[]) => elements.map((el: { innerText: any; }) => el.innerText))
+            sellPrice = await page.$$eval('.tile.is-parent.is-5 div.values div.venta div.val', (elements: any[]) => elements.map((el: { innerText: any; }) => el.innerText))
+
+            buy_price = parseFloat(buyPrice[0].replace('$', ''))
+            sell_price = parseFloat(sellPrice[0].replace('$', ''))
+
+        } else if (source === 'cronista') {
+
+            await page.goto('https://www.cronista.com/MercadosOnline/moneda.html?id=ARSB');
+            url = await page.url();
+
+            buyPrice = await page.$$eval('.piece.markets.standard.boxed .buy-value', (elements: any[]) => elements.map((el: { innerText: any; }) => el.innerText))
+            sellPrice = await page.$$eval('.piece.markets.standard.boxed .sell-value', (elements: any[]) => elements.map((el: { innerText: any; }) => el.innerText))
+
+            buy_price = parseFloat(buyPrice[0].replace('$', '').replace(',', '.'))
+            sell_price = parseFloat(sellPrice[0].replace('$', '').replace(',', '.'))
+
+        } else {
+            return res.send({
+                "Error": "Recurso no encontrado"
+            })
+        }
+
+        await browser.close();
+
+        return res.status(200).send({
+            "buy_price": buy_price,
+            "sell_price": sell_price,
+            "source": url
+        })
+
+    } catch (err) {
+        res.send({
+            Error: err
+        })
+    }
+}; */
+
 export const getQuotes = async (req: any, res: any) => {
 
     try {
@@ -123,137 +198,28 @@ export const getQuotes = async (req: any, res: any) => {
     }
 };
 
-export const getQuote = async (req: { query: { source: any; }; }, res: any) => {
-
-    const { source } = req.query;
-
-    try {
-
-        const browser = await puppeteer.launch({
-            args: ['--no-sandbox']
-        });
-
-        const page = await browser.newPage();
-        await page.setDefaultNavigationTimeout(0);
-
-        let buyPrice;
-        let sellPrice;
-        let url;
-
-        let buy_price;
-        let sell_price;
-
-        if (source === 'ambito') {
-
-            await page.goto('https://www.ambito.com/contenidos/dolar.html');
-            url = await page.url();
-
-            buyPrice = await page.$$eval('[data-indice*="/dolar/informal"] span.value.data-compra', (elements: any[]) => elements.map((el: { innerText: any; }) => el.innerText))
-            sellPrice = await page.$$eval('[data-indice*="/dolar/informal"] span.value.data-venta', (elements: any[]) => elements.map((el: { innerText: any; }) => el.innerText))
-
-            buy_price = parseFloat(buyPrice[0].replace(',', '.'))
-            sell_price = parseFloat(sellPrice[0].replace(',', '.'))
-
-        } else if (source === 'dolarhoy') {
-
-            await page.goto('https://www.dolarhoy.com');
-            url = await page.url();
-
-            buyPrice = await page.$$eval('.tile.is-parent.is-5 div.values div.compra div.val', (elements: any[]) => elements.map((el: { innerText: any; }) => el.innerText))
-            sellPrice = await page.$$eval('.tile.is-parent.is-5 div.values div.venta div.val', (elements: any[]) => elements.map((el: { innerText: any; }) => el.innerText))
-
-            buy_price = parseFloat(buyPrice[0].replace('$', ''))
-            sell_price = parseFloat(sellPrice[0].replace('$', ''))
-
-        } else if (source === 'cronista') {
-
-            await page.goto('https://www.cronista.com/MercadosOnline/moneda.html?id=ARSB');
-            url = await page.url();
-
-            buyPrice = await page.$$eval('.piece.markets.standard.boxed .buy-value', (elements: any[]) => elements.map((el: { innerText: any; }) => el.innerText))
-            sellPrice = await page.$$eval('.piece.markets.standard.boxed .sell-value', (elements: any[]) => elements.map((el: { innerText: any; }) => el.innerText))
-
-            buy_price = parseFloat(buyPrice[0].replace('$', '').replace(',', '.'))
-            sell_price = parseFloat(sellPrice[0].replace('$', '').replace(',', '.'))
-
-        } else {
-            return res.send({
-                "Error": "Recurso no encontrado"
-            })
-        }
-
-        await browser.close();
-
-        return res.status(200).send({
-            "buy_price": buy_price,
-            "sell_price": sell_price,
-            "source": url
-        })
-
-    } catch (err) {
-        res.send({
-            Error: err
-        })
-    }
-};
-
 export const getAverage = async (req: any, res: any) => {
 
     try {
 
-        const browser = await puppeteer.launch({
-            args: ['--no-sandbox']
-        });
-
-        const page = await browser.newPage();
-        await page.setDefaultNavigationTimeout(0);
-
-        const buyPrices = [];
-        const sellPrices = [];
+        let quotes;
 
         try {
 
-            await page.goto('https://www.ambito.com/contenidos/dolar.html');
-
-            const buyPrice = await page.$$eval('[data-indice*="/dolar/informal"] span.value.data-compra', (elements: any[]) => elements.map((el: { innerText: any; }) => el.innerText))
-            const sellPrice = await page.$$eval('[data-indice*="/dolar/informal"] span.value.data-venta', (elements: any[]) => elements.map((el: { innerText: any; }) => el.innerText))
-
-            buyPrices.push(parseFloat(buyPrice[0].replace(',', '.')))
-            sellPrices.push(parseFloat(sellPrice[0].replace(',', '.')))
+            const { data } = await axios.get(`${process.env.API_DEV}/quotes`);
+            quotes = data;
 
         } catch (err) {
             console.log(err)
         }
 
-        try {
+        const buyPrices = quotes.map((e: any) => {
+            return e.buy_price;
+        })
 
-            await page.goto('https://www.dolarhoy.com');
-
-            const buyPrice = await page.$$eval('.tile.is-parent.is-5 div.values div.compra div.val', (elements: any[]) => elements.map((el: { innerText: any; }) => el.innerText))
-            const sellPrice = await page.$$eval('.tile.is-parent.is-5 div.values div.venta div.val', (elements: any[]) => elements.map((el: { innerText: any; }) => el.innerText))
-
-            buyPrices.push(parseFloat(buyPrice[0].replace('$', '')))
-            sellPrices.push(parseFloat(sellPrice[0].replace('$', '')))
-
-        } catch (err) {
-            console.log(err)
-        }
-
-        try {
-
-            await page.goto('https://www.cronista.com/MercadosOnline/moneda.html?id=ARSB');
-
-            const buyPrice = await page.$$eval('.piece.markets.standard.boxed .buy-value', (elements: any[]) => elements.map((el: { innerText: any; }) => el.innerText))
-            const sellPrice = await page.$$eval('.piece.markets.standard.boxed .sell-value', (elements: any[]) => elements.map((el: { innerText: any; }) => el.innerText))
-
-            buyPrices.push(parseFloat(buyPrice[0].replace('$', '').replace(',', '.')))
-            sellPrices.push(parseFloat(sellPrice[0].replace('$', '').replace(',', '.')))
-
-        } catch (err) {
-            console.log(err)
-        }
-
-        await browser.close();
+        const sellPrices = quotes.map((e: any) => {
+            return e.sell_price;
+        })
 
         const getAverage = (array: any[]) => {
             let sum = array.reduce((prev: any, curr: any) => prev + curr, 0)
