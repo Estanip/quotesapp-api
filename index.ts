f263b7cf484082efb098355beee86e6f7bc4b307
Estanip <estani.pettigrew@gmail.com>
2022-05-05 18:35:12 -0300

    testing deploy

--- index.ts
+++ index.ts
@@ -7,7 +7,7 @@ const app = express();
 
 // Settings
 const corsOptions ={
-    origin:'http://localhost:3000', 
+    origin:'*', 
     credentials:true,            //access-control-allow-credentials:true
     optionSuccessStatus:200
 }
