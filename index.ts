08a8645b9c5f6f38092738bf0d5e285f82bb4b05
Estanip <estani.pettigrew@gmail.com>
2022-05-05 21:15:06 -0300

    testing deploy

--- index.ts
+++ index.ts
@@ -6,13 +6,28 @@ const path = require('path');
 const app = express();
 
 // Settings
-const corsOptions ={
-    origin:'*', 
-    optionSuccessStatus:200
-}
-app.use(cors(corsOptions));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use((req: any, res: any, next: any) => {
+    res.setHeader("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
+    res.setHeader("Access-Control-Allow-Credentials", "true");
+    res.setHeader(
+        "Access-Control-Allow-Headers",
+        "Origin, X-Requested-With, Content-Type, Accept"
+    );
+    res.setHeader(
+        "Access-Control-Allow-Methods",
+        "GET, POST, OPTIONS, PUT, DELETE, PATCH"
+    );
+    next();
+});
+app.get('/cors', (req:any, res:any) => {
+    res.set('Access-Control-Allow-Origin', '*');
+    res.send({ "msg": "This has CORS enabled 🎈" })
+})
+
+app.use(cors({ origin: "*" }));
+
 
 // Routes
 app.use('/api', require(path.join(__dirname, '/src/routes/quotes')));
