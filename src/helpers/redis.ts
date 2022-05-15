f57debf4526622af2a38611435fa91bfb9a26094
Estanip <estani.pettigrew@gmail.com>
2022-05-15 14:59:12 -0300

    heroku redis working

--- src/helpers/redis.ts
+++ src/helpers/redis.ts
@@ -5,12 +5,7 @@ const client = redis.createClient({url: process.env.REDISCLOUD_URL});
 
 (async function () {
 
-    console.log(process.env.REDISCLOUD_URL)
-    console.log(typeof(process.env.REDISCLOUD_URL))
-
-
     client.on('error', (err: Error) => console.log('Redis Client Error', err));
-
     await client.connect();
 
 })();
